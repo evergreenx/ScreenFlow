780b0d5c2cff71c74cbd5e6b9322466a124b4617
ido evergeen <dicksonevergreenido@gmail.com>
2023-05-08 22:38:33 +0100

    fix popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -2,6 +2,7 @@
 
 import { motion, Variants } from "framer-motion";
 import { fullscreenIcon, ghIcon, settingsIcon, tabIcon } from "../assets";
+import { useState } from "react";
 
 const itemVariants: Variants = {
   hidden: { opacity: 0, y: -10 },
@@ -9,44 +10,45 @@ const itemVariants: Variants = {
 };
 
 export default function PopUp() {
-//   async function getCurrentTab() {
-//     // const queryOptions = { active: true, lastFocusedWindow: true };
-//     // // `tab` will either be a `tabs.Tab` instance or `undefined`.
-//     // const [tab] = await chrome.tabs.query(queryOptions);
+  //   async function getCurrentTab() {
+  //     // const queryOptions = { active: true, lastFocusedWindow: true };
+  //     // // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  //     // const [tab] = await chrome.tabs.query(queryOptions);
 
-//     // console.log(tab);
-//     // return tab;
+  //     // console.log(tab);
+  //     // return tab;
 
-//     const [tab] = await chrome.tabs.query({ active: true });
+  //     const [tab] = await chrome.tabs.query({ active: true });
 
-//     chrome.scripting.executeScript({
-//       target: { tabId: tab.id! },
+  //     chrome.scripting.executeScript({
+  //       target: { tabId: tab.id! },
 
-//       func: () => {
-//         alert("hello from extention");
-//       },
-//     });
+  //       func: () => {
+  //         alert("hello from extention");
+  //       },
+  //     });
 
-//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//         // `tabs` is an array of tabs that are currently active in the current window.
-//         // The first tab in the array is the current active tab.
-//         const currentTab = tabs[0];
-      
-//         // You can access the title property of the current tab.
-//         const tabTitle = currentTab.title;
-      
-//         console.log("Current tab title:", tabTitle);
-//       });
-//   }
+  //     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //         // `tabs` is an array of tabs that are currently active in the current window.
+  //         // The first tab in the array is the current active tab.
+  //         const currentTab = tabs[0];
 
+  //         // You can access the title property of the current tab.
+  //         const tabTitle = currentTab.title;
 
+  //         console.log("Current tab title:", tabTitle);
+  //       });
+  //   }
 
+  const [countdown, setCountdown] = useState(0); // Initial countdown time
+  const [recording, setRecording] = useState(false);
 
-
-
-const startRecording = async () => {
+  const startRecording = async () => {
     try {
-      const stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true,
+      });
       const mediaRecorder = new MediaRecorder(stream);
       const chunks: BlobPart[] = [];
 
@@ -58,26 +60,43 @@ const startRecording = async () => {
 
       mediaRecorder.onstop = () => {
         const blob = new Blob(chunks, { type: 'video/webm' });
-        const url :string = URL.createObjectURL(blob);
+        const url: string = URL.createObjectURL(blob);
 
-        console.log(url)
         // You can do something with the recorded video, e.g., display it or save it.
+
+        // Create a download link for the recorded video
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recorded-video.webm';
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
 
         // Clean up resources
         stream.getTracks().forEach((track) => track.stop());
         chunks.length = 0;
       };
 
-      mediaRecorder.start();
+      setRecording(true);
 
+      // Start the countdown
+      const countdownInterval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+        if (countdown === 1) {
+          clearInterval(countdownInterval);
+          mediaRecorder.start();
+        }
+      }, 1000);
+
+      // Stop recording after a specified duration (adjust as needed).
       setTimeout(() => {
         mediaRecorder.stop();
-      }, 5000); // Stop recording after 5 seconds (adjust as needed).
+      }, 5000);
     } catch (error) {
       console.error('Error accessing user media:', error);
     }
   };
-
 
   return (
     <>
@@ -140,13 +159,24 @@ const startRecording = async () => {
           </div>
         </motion.div>
 
-        <motion.button
-          variants={itemVariants}
-          onClick={startRecording}
-          className="text-base font-medium bg-primary p-4 w-full rounded-[8px] text-[#FAFDFF] my-4  "
-        >
-          Start Recording
-        </motion.button>
+        {countdown > 0 && !recording ? (
+          <div className="countdown">
+            <motion.p
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+            >
+              {countdown}
+            </motion.p>
+          </div>
+        ) : (
+          <motion.button
+            variants={itemVariants}
+            onClick={startRecording}
+            className="text-base font-medium bg-primary p-4 w-full rounded-[8px] text-[#FAFDFF] my-4"
+          >
+            Start Recording
+          </motion.button>
+        )}
       </motion.div>
     </>
   );
