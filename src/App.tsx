d049bffd07eedc860c711a6a123ed3bf213445a6
ido evergeen <dicksonevergreenido@gmail.com>
2023-05-08 22:43:24 +0100

    testing app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import PopUp from "./components/Popup";
 function App() {
  
   return (
-    <>
+    <>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos soluta, asperiores animi error, nesciunt temporibus harum dicta voluptas velit et nisi necessitatibus eveniet enim officia illo impedit natus doloremque!
       <PopUp />
     </>
   );
